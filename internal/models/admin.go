71571d4860767f98b9c04238084903adbbccc400
Thoufeeq <connectthoufeeq@gmail.com>
2025-10-04 12:52:43 +0530

    admin model updated

--- internal/models/admin.go
+++ internal/models/admin.go
@@ -5,13 +5,14 @@ import "time"
 // manager && staff && admin
 
 type Admin struct {
-	ID          string `json:"id"`
-	Name        string `json:"name"`
-	Email       string `json:"email"`
-	PhoneNumber string `json:"phonenumber"`
-	Password    string `json:"password"`
-	Role        string `json:"role"`
-	Block       bool   `gorm:"default:false"`
+	ID          string   `json:"id"`
+	Name        string   `json:"name"`
+	Email       string   `json:"email"`
+	PhoneNumber string   `json:"phonenumber"`
+	Password    string   `json:"password"`
+	Role        string   `json:"role"`
+	Permissions []string `gorm:"type:text"`
+	Block       bool     `gorm:"default:false"`
 	CreatedAt   time.Time
 	DeletedAt   time.Time
 }
