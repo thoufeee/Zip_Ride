8ac62a6916caeb88f2d1038d349ada3b1af02530
RoshithBalachandran <roshith9745@gmail.com>
2025-10-06 15:10:12 +0530

    staff  management admin CURD

--- internal/domain/user_Admin/controllers/delete_sataff.go
+++ internal/domain/user_Admin/controllers/delete_sataff.go
@@ -4,15 +4,18 @@ import (
 	"net/http"
 	"zipride/database"
 	"zipride/internal/constants"
-	"zipride/internal/middleware"
 	"zipride/internal/models"
 
 	"github.com/gin-gonic/gin"
 )
 
 func DeleteStaff(c *gin.Context) {
-	//getting staff id 
-	staffid := middleware.GetUserID(c)
+	//getting staff id from param
+	staffid := c.Param("id")
+	if staffid == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "staff id Required"})
+		return
+	}
 	//fetch staff from database
 	var staff models.Admin
 	if err := database.DB.First("id = ? AND RoleID = ?", staffid, constants.RoleStaff).Find(&staff).Error; err != nil {
@@ -25,7 +28,5 @@ func DeleteStaff(c *gin.Context) {
 		return
 	}
 	// sucess messge
-	c.JSON(http.StatusOK, gin.H{"message": "Staff deleted sucessfully",
-		"staff": staff,
-	})
+	c.JSON(http.StatusOK, gin.H{"message": "Staff deleted sucessfully",})
 }
