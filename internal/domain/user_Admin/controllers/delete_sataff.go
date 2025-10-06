24bb2a25ec9fae624d6ceb0182d6e52502946a73
RoshithBalachandran <roshith9745@gmail.com>
2025-10-06 12:38:23 +0530

    Staff management CURD

---
+++ internal/domain/user_Admin/controllers/delete_sataff.go
@@ -0,0 +1,31 @@
+package controllers
+
+import (
+	"net/http"
+	"zipride/database"
+	"zipride/internal/constants"
+	"zipride/internal/middleware"
+	"zipride/internal/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func DeleteStaff(c *gin.Context) {
+	//getting staff id 
+	staffid := middleware.GetUserID(c)
+	//fetch staff from database
+	var staff models.Admin
+	if err := database.DB.First("id = ? AND RoleID = ?", staffid, constants.RoleStaff).Find(&staff).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "staff not found"})
+		return
+	}
+	//delete staff data
+	if err := database.DB.Delete(&staff).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete staff"})
+		return
+	}
+	// sucess messge
+	c.JSON(http.StatusOK, gin.H{"message": "Staff deleted sucessfully",
+		"staff": staff,
+	})
+}
--- internal/domain/user_Admin/controllers/staff_create.go
+++ internal/domain/user_Admin/controllers/staff_create.go
@@ -2,24 +2,65 @@ package controllers
 
 import (
 	"net/http"
+	"zipride/database"
+	"zipride/internal/constants"
+	"zipride/internal/models"
+	"zipride/utils"
 
 	"github.com/gin-gonic/gin"
 )
 
 // creating staff || manager
-
 func CreateStaff(c *gin.Context) {
 
-	var data struct {
-		Name        string `json:"name" binding:"required"`
-		Email       string `json:"email" binding:"required"`
-		PhoneNumber string `json:"phonenumber" binding:"required"`
-		Password    string `json:"password" binding:"required"`
-		Role        string `json:"role" binding:"required"`
-	}
-
+		var data struct {
+			Name        string `json:"name" binding:"required"`
+			Email       string `json:"email" binding:"required"`
+			PhoneNumber string `json:"phonenumber" binding:"required"`
+			Password    string `json:"password" binding:"required"`
+			Role        string `json:"role" binding:"required"`
+		}
+	//Get Staff details
 	if err := c.ShouldBindJSON(&data); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"err": "fill blanks"})
 		return
 	}
+	//check if staff email alredy exist
+	var existingstaff models.Admin
+	if err := database.DB.Find("email=?", data.Email).First(&existingstaff).Error; err != nil {
+		c.JSON(http.StatusConflict, gin.H{"error": "Email alredy exist"})
+		return
+	}
+	//checking emaail
+	if !utils.EmailCheck(data.Email) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid email format "})
+		return
+	}
+	//veryfy phone Number
+	if !utils.PhoneNumberCheck(data.PhoneNumber) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid phone Number"})
+		return
+	}
+	//password Secure
+	hash, err := utils.GenerateHash(data.Password)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed while hashing password"})
+		return
+	}
+	// staff details to store on database
+	staff := models.Admin{
+		Name:        data.Name,
+		Email:       data.Email,
+		PhoneNumber: data.PhoneNumber,
+		Password:    hash,
+		RoleID:      constants.RoleStaff,
+	}
+	//Storing new staff data to database
+	if err := database.DB.Create(&staff).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create staff"})
+		return
+	}
+	//sucess responce
+	c.JSON(http.StatusOK, gin.H{"message": "Staff created sucessfully",
+		"staff": staff})
 }
