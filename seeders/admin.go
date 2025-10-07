a781f0cdc9bef43417e7961c4c0547f70337c326
Thoufeeq <connectthoufeeq@gmail.com>
2025-10-07 18:48:12 +0530

    adding admin updated

--- seeders/admin.go
+++ seeders/admin.go
@@ -16,10 +16,24 @@ func SeedAdmin() {
 	adminEmail := os.Getenv("EMAIL")
 	adminPassword := os.Getenv("PASSWORD")
 
-	var admin models.User
+	var admin models.Admin
 
 	if err := database.DB.Where("email = ?", adminEmail).First(&admin).Error; err == nil {
 		log.Println("admin already exist")
+		return
+	}
+
+	// super admin role
+	var supreRole models.Role
+	if err := database.DB.Where("name = ?", constants.RoleSuperAdmin).First(&supreRole).Error; err != nil {
+		log.Fatal("super admin role not found")
+		return
+	}
+
+	// permissions
+	var permissions []models.Permission
+	if err := database.DB.Find(&permissions).Error; err != nil {
+		log.Fatal("failed to fetch permissions")
 		return
 	}
 
@@ -28,10 +42,11 @@ func SeedAdmin() {
 		log.Fatal("failed to hash adminpass")
 	}
 
-	newadmin := &models.User{
-		Email:    adminEmail,
-		Password: hashpass,
-		Role:     constants.RoleSuperAdmin,
+	newadmin := &models.Admin{
+		Email:       adminEmail,
+		Password:    hashpass,
+		Permissions: permissions,
+		RoleID:      supreRole.ID,
 	}
 
 	if err := database.DB.Create(&newadmin).Error; err != nil {
