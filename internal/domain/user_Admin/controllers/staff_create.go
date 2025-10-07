26130efa40147ba2694eb1c8ab313e69631bf1a8
Thoufeeq <connectthoufeeq@gmail.com>
2025-10-07 10:06:08 +0530

    staff and admin creatin updated

--- internal/domain/user_Admin/controllers/staff_create.go
+++ internal/domain/user_Admin/controllers/staff_create.go
@@ -2,6 +2,9 @@ package controllers
 
 import (
 	"net/http"
+	"zipride/database"
+	"zipride/internal/models"
+	"zipride/utils"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,15 +14,51 @@ import (
 func CreateStaff(c *gin.Context) {
 
 	var data struct {
-		Name        string `json:"name" binding:"required"`
-		Email       string `json:"email" binding:"required"`
-		PhoneNumber string `json:"phonenumber" binding:"required"`
-		Password    string `json:"password" binding:"required"`
-		Role        string `json:"role" binding:"required"`
+		Name        string   `json:"name" binding:"required"`
+		Email       string   `json:"email" binding:"required"`
+		PhoneNumber string   `json:"phonenumber" binding:"required"`
+		Password    string   `json:"password" binding:"required"`
+		Role        string   `json:"role"`
+		ExtraPerms  []string `json:"extra_permissions"`
 	}
 
 	if err := c.ShouldBindJSON(&data); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"err": "fill blanks"})
+		c.JSON(http.StatusBadRequest, gin.H{"err": "invalid request"})
 		return
 	}
+
+	// finding role
+	var role models.Role
+
+	if err := database.DB.Where("name = ?", data.Name).Preload("Permissions").First(&role).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"err": "role not found"})
+		return
+	}
+
+	// for extra permissions
+	var Permission []models.Permission
+
+	if len(Permission) > 0 {
+		database.DB.Where("name IN", data.ExtraPerms).Find(&Permission)
+	}
+
+	allPermission := append(role.Permissions, Permission...)
+
+	hashpass, _ := utils.GenerateHash(data.Password)
+
+	newStaff := models.Admin{
+		Name:        data.Name,
+		Email:       data.Email,
+		PhoneNumber: data.PhoneNumber,
+		Password:    hashpass,
+		RoleID:      &role.ID,
+		Permissions: allPermission,
+	}
+
+	if err := database.DB.Create(&newStaff).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"err": "creation failed"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"res": "Successfuly created", "": data.Role})
 }
