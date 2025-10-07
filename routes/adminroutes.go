3f2d1fad35b5e4e452343597b4ff826328a94b73
RoshithBalachandran <roshith9745@gmail.com>
2025-10-07 09:51:46 +0530

    Save work before syncing

--- routes/adminroutes.go
+++ routes/adminroutes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"zipride/internal/constants"
+	"zipride/internal/domain/user_Admin/controllers"
 	"zipride/internal/domain/user_Admin/services"
 	"zipride/internal/middleware"
 
@@ -17,4 +18,16 @@ func AdminRoutes(c *gin.Engine) {
 	admin.Use(middleware.Auth(constants.RoleSuperAdmin))
 
 	admin.GET("/allusers", services.GetAllUsers)
+
+	//Staff management
+	admin.GET("/staffs", controllers.GETAllStaff)
+	admin.POST("/createstaff", controllers.CreateStaff)
+	admin.PUT("/staff/:id", controllers.UpdateStaff)
+	admin.DELETE("/staff/:id", controllers.UpdateStaff)
+
+	//user management
+	admin.GET("/users",services.GetAllUsers)
+	admin.POST("/createuser",services.AddUser)
+	admin.PUT("/user/:id",services.UpdateUser)
+	admin.DELETE("/user/:id",services.DeleteUser)
 }
