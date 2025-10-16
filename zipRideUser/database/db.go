f16d440fb6d00f73b75f678334a7798ab2d7eb8e
Thoufeeq <connectthoufeeq@gmail.com>
2025-10-16 14:02:13 +0530

    db updated

--- zipRideUser/database/db.go
+++ zipRideUser/database/db.go
@@ -27,7 +27,7 @@ func Connect() {
 		log.Fatal("failed to connect database")
 	}
 
-	err = DB.AutoMigrate(&models.Permission{}, &models.Role{}, &models.Admin{}, &models.User{},&models.Booking{})
+	err = DB.AutoMigrate(&models.Permission{}, &models.Role{}, &models.Admin{}, &models.User{}, &models.Booking{})
 
 	if err != nil {
 		log.Fatal("failed to automigrate")
