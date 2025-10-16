be1adb40406aa35e619523a3f05b8eea6acf7b74
Thoufeeq <connectthoufeeq@gmail.com>
2025-10-16 14:07:48 +0530

    cmd added

---
+++ zipRideUser/cmd/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+	"time"
+	"zipride/database"
+	"zipride/routes"
+	"zipride/seeders"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	// env load
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatal("failed to load env")
+	}
+
+	// database connect
+	database.Connect()
+
+	// redis connect
+	database.InitRedis()
+
+	// seeders run
+	seeders.RunAllSeeders()
+
+	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://127.0.0.1:5500", "http://localhost:5500"},
+		AllowMethods:     []string{"POST", "GET", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	// routes connection
+	routes.PublicRoutes(r)
+	routes.UserRoutes(r)
+	routes.SuperAdminRoutes(r)
+
+	r.Run(":8080")
+}
