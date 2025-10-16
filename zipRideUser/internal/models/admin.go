e3dbda44f8ac230290d5e8ebd3c757a0834fad76
Thoufeeq <connectthoufeeq@gmail.com>
2025-10-16 14:22:06 +0530

    models added

---
+++ zipRideUser/internal/models/admin.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+// manager && staff && admin
+
+type Admin struct {
+	gorm.Model
+	Name        string       `json:"name"`
+	Email       string       `json:"email"`
+	PhoneNumber string       `json:"phonenumber"`
+	Password    string       `json:"password"`
+	RoleID      uint         `json:"role_id"`
+	Role        Role         `json:"role"`
+	Permissions []Permission `gorm:"many2many:admin_permissions;"`
+	Block       bool         `gorm:"default:false"`
+}
