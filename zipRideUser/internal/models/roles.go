00e5483a11494557be05a2c7b52d7cfce32d252a
Thoufeeq <connectthoufeeq@gmail.com>
2025-10-16 12:28:55 +0530

    Delete zipRideUser/internal/models/roles.go

--- zipRideUser/internal/models/roles.go
+++
@@ -1,9 +0,0 @@
-package models
-
-// roles
-
-type Role struct {
-	ID          uint         `gorm:"primarykey"`
-	Name        string       `json:"name"`
-	Permissions []Permission `gorm:"many2many:role_permissions;"`
-}
