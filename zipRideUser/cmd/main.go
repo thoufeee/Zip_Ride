0e12795ebdaca914db520a0d7df15d50ed0d3c29
Thoufeeq <connectthoufeeq@gmail.com>
2025-10-20 15:09:34 +0530

    cmd updated

--- zipRideUser/cmd/main.go
+++ zipRideUser/cmd/main.go
@@ -36,7 +36,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://127.0.0.1:5500", "http://localhost:5500"},
+		AllowOrigins:     []string{"http://127.0.0.1:5500", "http://localhost:3000"},
 		AllowMethods:     []string{"POST", "GET", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
