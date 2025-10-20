5993dddc5d35fb4f94b44c39f39fd4c328b29b87
Thoufeeq <connectthoufeeq@gmail.com>
2025-10-20 14:33:55 +0530

    file name changed

---
+++ zipRideUser/internal/domain/user_Admin/staffManagment/controllers/delete_staff.go
@@ -0,0 +1,41 @@
+package controllers
+
+import (
+	"net/http"
+	"strconv"
+	"zipride/database"
+	"zipride/internal/constants"
+	"zipride/internal/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func DeleteStaff(c *gin.Context) {
+	//getting staff id from param
+	staffid := c.Param("id")
+	if staffid == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "staff id Required"})
+		return
+	}
+
+	staffID, err := strconv.ParseUint(staffid, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid staff id"})
+		return
+	}
+
+	//fetch staff from database
+	var staff models.Admin
+	if err := database.DB.Where("id = ? AND role = ?", staffID, constants.RoleAdmin).First(&staff).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "staff not found"})
+		return
+	}
+
+	//delete staff data
+	if err := database.DB.Delete(&staff).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete staff"})
+		return
+	}
+	// sucess messge
+	c.JSON(http.StatusOK, gin.H{"message": "Staff deleted sucessfully"})
+}
