ef392daea3aaa59bb13df30f27d5bd66296cb2da
Thoufeeq <connectthoufeeq@gmail.com>
2025-10-20 14:35:12 +0530

    update staff updated

--- zipRideUser/internal/domain/user_Admin/staffManagment/controllers/update_staff.go
+++ zipRideUser/internal/domain/user_Admin/staffManagment/controllers/update_staff.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
 	"zipride/database"
 	"zipride/internal/constants"
@@ -10,6 +11,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// update admin profile
 func UpdateStaff(c *gin.Context) {
 	staffID := c.Param("id")
 	if staffID == "" {
@@ -18,16 +20,17 @@ func UpdateStaff(c *gin.Context) {
 	}
 
 	var staff models.Admin
-	if err := database.DB.Where("id = ? AND role_id = ?", staffID, constants.Staff).First(&staff).Error; err != nil {
+	if err := database.DB.Where("id = ? AND role = ?", staffID, constants.RoleAdmin).First(&staff).Error; err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Staff not found"})
 		return
 	}
 
 	var input struct {
-		Name        string `json:"name" binding:"required"`
-		Email       string `json:"email" binding:"required"`
-		PhoneNumber string `json:"phonenumber" binding:"required"`
-		Password    string `json:"password"`
+		Name        string   `json:"name"`
+		Email       string   `json:"email"`
+		PhoneNumber string   `json:"phonenumber"`
+		Password    string   `json:"password"`
+		ExtraPerms  []string `json:"extra_permissions"`
 	}
 
 	if err := c.ShouldBindJSON(&input); err != nil {
@@ -58,10 +61,51 @@ func UpdateStaff(c *gin.Context) {
 		staff.Password = hashed
 	}
 
-	// Update fields
-	staff.Name = input.Name
-	staff.Email = input.Email
-	staff.PhoneNumber = phone
+	if input.Name != "" {
+		staff.Name = input.Name
+	}
+
+	if input.Email != "" {
+		staff.Email = input.Email
+	}
+
+	if input.PhoneNumber != "" {
+		staff.PhoneNumber = input.PhoneNumber
+	}
+
+	if len(input.ExtraPerms) > 0 {
+		var existing []string
+
+		if err := json.Unmarshal([]byte(staff.Permissions), &existing); err != nil {
+			existing = []string{}
+		}
+
+		perMap := make(map[string]bool)
+		for _, p := range existing {
+			{
+				perMap[p] = true
+			}
+		}
+
+		for _, p := range input.ExtraPerms {
+			if p != "Click an available permission to add it." {
+				perMap[p] = true
+			}
+		}
+
+		merged := []string{}
+		for p := range perMap {
+			merged = append(merged, p)
+		}
+
+		permjson, err := json.Marshal(merged)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"err": "Failed to process permissions"})
+			return
+		}
+
+		staff.Permissions = permjson
+	}
 
 	if err := database.DB.Save(&staff).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update staff"})
