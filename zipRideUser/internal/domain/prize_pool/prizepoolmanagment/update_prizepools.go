d111f3d3c55c2fc2feb81a100751aaef16bde3c8
Thoufeeq <connectthoufeeq@gmail.com>
2025-10-22 18:15:36 +0530

    added

---
+++ zipRideUser/internal/domain/prize_pool/prizepoolmanagment/update_prizepools.go
@@ -0,0 +1,52 @@
+package prizepoolmanagment
+
+import (
+	"net/http"
+	"zipride/database"
+	"zipride/internal/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+// update prize pool
+
+func UpdatePrizePool(c *gin.Context) {
+	id := c.Param("id")
+
+	var prizepool models.PrizePool
+
+	if err := database.DB.Where("id = ?", id).First(&prizepool).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"err": "no prize pool found"})
+		return
+	}
+
+	var input struct {
+		VehicleType string   `json:"vehicle_type"`
+		Commission  *float64 `json:"commission"`
+		BonusAmount *float64 `json:"bonusamount"`
+		Active      *bool    `json:"active"`
+	}
+
+	if input.VehicleType != "" {
+		prizepool.VehicleType = input.VehicleType
+	}
+
+	if input.Commission != nil {
+		prizepool.Commission = *input.Commission
+	}
+
+	if input.BonusAmount != nil {
+		prizepool.BonusAmount = *input.BonusAmount
+	}
+
+	if input.Active != nil {
+		prizepool.Active = *input.Active
+	}
+
+	if err := database.DB.Save(&prizepool).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"err": "failed to update prize pool"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"res": "succsessfuly updated prize pool"})
+}
