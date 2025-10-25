7e922410967c30d05c9f2d62293af00b15cfda92
Thoufeeq <connectthoufeeq@gmail.com>
2025-10-25 12:05:04 +0530

    updated

--- zipRideUser/internal/models/admin.go
+++ zipRideUser/internal/models/admin.go
@@ -9,7 +9,6 @@ import (
 
 type Admin struct {
 	gorm.Model
-<<<<<<< HEAD
 	Name        string         `json:"name"`
 	Email       string         `json:"email"`
 	PhoneNumber string         `json:"phonenumber"`
@@ -17,13 +16,4 @@ type Admin struct {
 	Role        string         `json:"role"`
 	Permissions datatypes.JSON `json:"permissions" gorm:"type:json"`
 	Block       bool           `gorm:"default:false"`
-=======
-	Name        string       `json:"name"`
-	Email       string       `json:"email"`
-	PhoneNumber string       `json:"phonenumber"`
-	Password    string       `json:"password"`
-	Role        string       `json:"role"`
-	Permissions []Permission `gorm:"many2many:admin_permissions;"`
-	Block       bool         `gorm:"default:false"`
->>>>>>> 2c00f30 (folders changed)
 }
